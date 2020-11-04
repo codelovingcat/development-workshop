50b5f9dc7853bc120d142e084ca12fbbe2d232ee
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-04 23:54:07 +0200

    desktop-yks

---
+++ desktop-ygs-point/Form1.Designer.cs
@@ -0,0 +1,317 @@
+﻿namespace desktop_ygs_point
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblYgs6 = new System.Windows.Forms.Label();
+            this.lblYgs5 = new System.Windows.Forms.Label();
+            this.lblYgs4 = new System.Windows.Forms.Label();
+            this.lblYgs3 = new System.Windows.Forms.Label();
+            this.lblYgs2 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblYgs1 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.txtFen = new System.Windows.Forms.TextBox();
+            this.txtSos = new System.Windows.Forms.TextBox();
+            this.txtMat = new System.Windows.Forms.TextBox();
+            this.txtTurkce = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnResult = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblYgs6
+            // 
+            this.lblYgs6.AutoSize = true;
+            this.lblYgs6.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYgs6.Location = new System.Drawing.Point(425, 206);
+            this.lblYgs6.Name = "lblYgs6";
+            this.lblYgs6.Size = new System.Drawing.Size(33, 29);
+            this.lblYgs6.TabIndex = 41;
+            this.lblYgs6.Text = "00";
+            // 
+            // lblYgs5
+            // 
+            this.lblYgs5.AutoSize = true;
+            this.lblYgs5.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYgs5.Location = new System.Drawing.Point(425, 176);
+            this.lblYgs5.Name = "lblYgs5";
+            this.lblYgs5.Size = new System.Drawing.Size(33, 29);
+            this.lblYgs5.TabIndex = 40;
+            this.lblYgs5.Text = "00";
+            // 
+            // lblYgs4
+            // 
+            this.lblYgs4.AutoSize = true;
+            this.lblYgs4.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYgs4.Location = new System.Drawing.Point(425, 149);
+            this.lblYgs4.Name = "lblYgs4";
+            this.lblYgs4.Size = new System.Drawing.Size(33, 29);
+            this.lblYgs4.TabIndex = 39;
+            this.lblYgs4.Text = "00";
+            // 
+            // lblYgs3
+            // 
+            this.lblYgs3.AutoSize = true;
+            this.lblYgs3.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYgs3.Location = new System.Drawing.Point(197, 206);
+            this.lblYgs3.Name = "lblYgs3";
+            this.lblYgs3.Size = new System.Drawing.Size(33, 29);
+            this.lblYgs3.TabIndex = 38;
+            this.lblYgs3.Text = "00";
+            // 
+            // lblYgs2
+            // 
+            this.lblYgs2.AutoSize = true;
+            this.lblYgs2.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYgs2.Location = new System.Drawing.Point(197, 176);
+            this.lblYgs2.Name = "lblYgs2";
+            this.lblYgs2.Size = new System.Drawing.Size(33, 29);
+            this.lblYgs2.TabIndex = 37;
+            this.lblYgs2.Text = "00";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label7.Location = new System.Drawing.Point(354, 206);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(76, 29);
+            this.label7.TabIndex = 36;
+            this.label7.Text = "YGS6 : ";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label8.Location = new System.Drawing.Point(354, 176);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(76, 29);
+            this.label8.TabIndex = 35;
+            this.label8.Text = "YGS5 : ";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label9.Location = new System.Drawing.Point(354, 149);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(76, 29);
+            this.label9.TabIndex = 34;
+            this.label9.Text = "YGS4 : ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.Location = new System.Drawing.Point(122, 206);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(76, 29);
+            this.label6.TabIndex = 33;
+            this.label6.Text = "YGS3 : ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(122, 176);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 29);
+            this.label5.TabIndex = 32;
+            this.label5.Text = "YGS2 : ";
+            // 
+            // lblYgs1
+            // 
+            this.lblYgs1.AutoSize = true;
+            this.lblYgs1.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblYgs1.Location = new System.Drawing.Point(197, 149);
+            this.lblYgs1.Name = "lblYgs1";
+            this.lblYgs1.Size = new System.Drawing.Size(33, 29);
+            this.lblYgs1.TabIndex = 31;
+            this.lblYgs1.Text = "00";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label11.Location = new System.Drawing.Point(122, 149);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(76, 29);
+            this.label11.TabIndex = 30;
+            this.label11.Text = "YGS1 : ";
+            // 
+            // txtFen
+            // 
+            this.txtFen.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtFen.Location = new System.Drawing.Point(417, 88);
+            this.txtFen.Name = "txtFen";
+            this.txtFen.Size = new System.Drawing.Size(103, 33);
+            this.txtFen.TabIndex = 29;
+            // 
+            // txtSos
+            // 
+            this.txtSos.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtSos.Location = new System.Drawing.Point(292, 88);
+            this.txtSos.Name = "txtSos";
+            this.txtSos.Size = new System.Drawing.Size(110, 33);
+            this.txtSos.TabIndex = 28;
+            // 
+            // txtMat
+            // 
+            this.txtMat.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtMat.Location = new System.Drawing.Point(170, 88);
+            this.txtMat.Name = "txtMat";
+            this.txtMat.Size = new System.Drawing.Size(105, 33);
+            this.txtMat.TabIndex = 27;
+            // 
+            // txtTurkce
+            // 
+            this.txtTurkce.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtTurkce.Location = new System.Drawing.Point(59, 88);
+            this.txtTurkce.Name = "txtTurkce";
+            this.txtTurkce.Size = new System.Drawing.Size(95, 33);
+            this.txtTurkce.TabIndex = 26;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(422, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 29);
+            this.label4.TabIndex = 25;
+            this.label4.Text = "Fen";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(287, 39);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 29);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Sosyal";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(165, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(110, 29);
+            this.label2.TabIndex = 23;
+            this.label2.Text = "Matematik";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(54, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 29);
+            this.label1.TabIndex = 22;
+            this.label1.Text = "Türkçe";
+            // 
+            // btnResult
+            // 
+            this.btnResult.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.btnResult.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnResult.Location = new System.Drawing.Point(224, 261);
+            this.btnResult.Name = "btnResult";
+            this.btnResult.Size = new System.Drawing.Size(131, 44);
+            this.btnResult.TabIndex = 21;
+            this.btnResult.Text = "Sonuç";
+            this.btnResult.UseVisualStyleBackColor = false;
+            this.btnResult.Click += new System.EventHandler(this.btnResult_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(606, 348);
+            this.Controls.Add(this.lblYgs6);
+            this.Controls.Add(this.lblYgs5);
+            this.Controls.Add(this.lblYgs4);
+            this.Controls.Add(this.lblYgs3);
+            this.Controls.Add(this.lblYgs2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblYgs1);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.txtFen);
+            this.Controls.Add(this.txtSos);
+            this.Controls.Add(this.txtMat);
+            this.Controls.Add(this.txtTurkce);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnResult);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblYgs6;
+        private System.Windows.Forms.Label lblYgs5;
+        private System.Windows.Forms.Label lblYgs4;
+        private System.Windows.Forms.Label lblYgs3;
+        private System.Windows.Forms.Label lblYgs2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblYgs1;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.TextBox txtFen;
+        private System.Windows.Forms.TextBox txtSos;
+        private System.Windows.Forms.TextBox txtMat;
+        private System.Windows.Forms.TextBox txtTurkce;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnResult;
+    }
+}
+
---
+++ desktop-ygs-point/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desktop_ygs_point
+{
+    public partial class Form1 : Form
+    {
+        double turkce, matematik, sosyal, fen;
+        double yks1, yks2, yks3, yks4, yks5, yks6;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnResult_Click(object sender, EventArgs e)
+        {
+            turkce = Convert.ToDouble(txtTurkce.Text);
+            matematik = Convert.ToDouble(txtMat.Text);
+            sosyal = Convert.ToDouble(txtSos.Text);
+            fen = Convert.ToDouble(txtFen.Text);
+
+            yks1 = 100.160 + (turkce * 1.999) + (sosyal * 1) + (matematik * 3.998) + (fen * 2.999);
+            lblYgs1.Text = yks1.ToString();
+
+            yks2 = 100.160 + (turkce * 1.999) + (sosyal * 1) + (matematik * 2.999) + (fen * 3.998);
+            lblYgs2.Text = yks2.ToString();
+
+            yks3 = 100.160 + (turkce * 3.998) + (sosyal * 2.999) + (matematik * 1.999) + (fen * 1);
+            lblYgs3.Text = yks3.ToString();
+
+            yks4 = 100.160 + (turkce * 2.999) + (sosyal * 3.998) + (matematik * 1.999) + (fen * 1);
+            lblYgs4.Text = yks4.ToString();
+
+            yks5 = 100.120 + (turkce * 3.699) + (sosyal * 1.999) + (matematik * 3.299) + (fen * 1);
+            lblYgs5.Text = yks5.ToString();
+
+            yks6 = 100.120 + (turkce * 3.299) + (sosyal * 1) + (matematik * 3.699) + (fen * 1.999);
+            lblYgs6.Text = yks6.ToString();
+        }
+    }
+}
