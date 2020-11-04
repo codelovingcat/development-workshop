56714d86dbabf6fbd617f7460a4a5b40709cad17
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-05 00:43:16 +0200

    desktop-stay-in-shape

---
+++ desktop-stay-in-shape/StayShape.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace desktop_stay_in_shape
+{
+    partial class StayShape
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.lblDurum = new System.Windows.Forms.Label();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.lblIndexSonuc = new System.Windows.Forms.Label();
+            this.lblIndex = new System.Windows.Forms.Label();
+            this.txtKilo = new System.Windows.Forms.TextBox();
+            this.txtBoy = new System.Windows.Forms.TextBox();
+            this.llblKilo = new System.Windows.Forms.Label();
+            this.lblBoy = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.btnHesapla.Font = new System.Drawing.Font("Minion Pro Med", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.ForeColor = System.Drawing.Color.White;
+            this.btnHesapla.Location = new System.Drawing.Point(442, 100);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(92, 47);
+            this.btnHesapla.TabIndex = 17;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = false;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // lblDurum
+            // 
+            this.lblDurum.AutoSize = true;
+            this.lblDurum.Font = new System.Drawing.Font("Minion Pro Med", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblDurum.Location = new System.Drawing.Point(316, 272);
+            this.lblDurum.Name = "lblDurum";
+            this.lblDurum.Size = new System.Drawing.Size(16, 19);
+            this.lblDurum.TabIndex = 16;
+            this.lblDurum.Text = "0";
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Minion Pro Med", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(97, 272);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(58, 19);
+            this.lblSonuc.TabIndex = 15;
+            this.lblSonuc.Text = "Sonuç : ";
+            // 
+            // lblIndexSonuc
+            // 
+            this.lblIndexSonuc.AutoSize = true;
+            this.lblIndexSonuc.Font = new System.Drawing.Font("Minion Pro Med", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblIndexSonuc.Location = new System.Drawing.Point(316, 214);
+            this.lblIndexSonuc.Name = "lblIndexSonuc";
+            this.lblIndexSonuc.Size = new System.Drawing.Size(16, 19);
+            this.lblIndexSonuc.TabIndex = 14;
+            this.lblIndexSonuc.Text = "0";
+            // 
+            // lblIndex
+            // 
+            this.lblIndex.AutoSize = true;
+            this.lblIndex.Font = new System.Drawing.Font("Minion Pro Med", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblIndex.Location = new System.Drawing.Point(97, 214);
+            this.lblIndex.Name = "lblIndex";
+            this.lblIndex.Size = new System.Drawing.Size(138, 19);
+            this.lblIndex.TabIndex = 13;
+            this.lblIndex.Text = "Vücut Kitle İndeksi : ";
+            // 
+            // txtKilo
+            // 
+            this.txtKilo.Font = new System.Drawing.Font("Minion Pro Med", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtKilo.Location = new System.Drawing.Point(242, 119);
+            this.txtKilo.Name = "txtKilo";
+            this.txtKilo.Size = new System.Drawing.Size(157, 33);
+            this.txtKilo.TabIndex = 12;
+            // 
+            // txtBoy
+            // 
+            this.txtBoy.Font = new System.Drawing.Font("Minion Pro Med", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtBoy.Location = new System.Drawing.Point(242, 83);
+            this.txtBoy.Name = "txtBoy";
+            this.txtBoy.Size = new System.Drawing.Size(157, 33);
+            this.txtBoy.TabIndex = 11;
+            // 
+            // llblKilo
+            // 
+            this.llblKilo.AutoSize = true;
+            this.llblKilo.Font = new System.Drawing.Font("Minion Pro Med", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.llblKilo.Location = new System.Drawing.Point(87, 119);
+            this.llblKilo.Name = "llblKilo";
+            this.llblKilo.Size = new System.Drawing.Size(124, 28);
+            this.llblKilo.TabIndex = 10;
+            this.llblKilo.Text = "Kilo (53,3) : ";
+            // 
+            // lblBoy
+            // 
+            this.lblBoy.AutoSize = true;
+            this.lblBoy.Font = new System.Drawing.Font("Minion Pro Med", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblBoy.Location = new System.Drawing.Point(86, 83);
+            this.lblBoy.Name = "lblBoy";
+            this.lblBoy.Size = new System.Drawing.Size(120, 28);
+            this.lblBoy.TabIndex = 9;
+            this.lblBoy.Text = "Boy (1,65) : ";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SkyBlue;
+            this.button1.Font = new System.Drawing.Font("Minion Pro Med", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(559, 100);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(92, 47);
+            this.button1.TabIndex = 18;
+            this.button1.Text = "Temizle";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // StayShape
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(727, 430);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lblDurum);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.lblIndexSonuc);
+            this.Controls.Add(this.lblIndex);
+            this.Controls.Add(this.txtKilo);
+            this.Controls.Add(this.txtBoy);
+            this.Controls.Add(this.llblKilo);
+            this.Controls.Add(this.lblBoy);
+            this.DoubleBuffered = true;
+            this.Name = "StayShape";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.Label lblDurum;
+        private System.Windows.Forms.Label lblSonuc;
+        private System.Windows.Forms.Label lblIndexSonuc;
+        private System.Windows.Forms.Label lblIndex;
+        private System.Windows.Forms.TextBox txtKilo;
+        private System.Windows.Forms.TextBox txtBoy;
+        private System.Windows.Forms.Label llblKilo;
+        private System.Windows.Forms.Label lblBoy;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ desktop-stay-in-shape/StayShape.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace desktop_stay_in_shape
+{
+    public partial class StayShape : Form
+    {
+        public StayShape()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            double kilo, boy, index;
+
+            boy = Convert.ToDouble(txtBoy.Text);
+            kilo = Convert.ToDouble(txtKilo.Text);
+
+            index = kilo / (boy * boy);
+
+            lblIndexSonuc.Text = index.ToString();
+
+            if (index < 18)
+            {
+                lblDurum.Text = "İstediğinizi yiyebilirsiniz. Çok Zayıfsınız.";
+            }
+            else if (index >= 18 && index < 25)
+            {
+                lblDurum.Text = "Henüz korkacak bir şey yok. Normalsiniz.";
+            }
+            else if (index >= 25 && index <= 30)
+            {
+                lblDurum.Text = "Spora ve diyete başlayın. Kilolusunuz.";
+            }
+            else if (index >= 30 && index <= 35)
+            {
+                lblDurum.Text = "Dikkat!!! Obezsiniz.";
+            }
+            else
+            {
+                lblDurum.Text = "Çok fena ciddi Obezsiniz!!!";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtBoy.Clear();
+            txtKilo.Clear();
+
+            
+            lblDurum.Text = "";
+            lblIndexSonuc.Text = "";
+          
+        }
+    }
+}
