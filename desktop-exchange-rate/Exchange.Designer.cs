9fc4af51118caf8721683596ddd6d81b88155ca0
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-10 00:58:55 +0200

    desktop-exchange-rate

---
+++ desktop-exchange-rate/Exchange.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace desktop_exchange_rate
+{
+    partial class Exchange
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnKur = new System.Windows.Forms.Button();
+            this.lbl1 = new System.Windows.Forms.Label();
+            this.lbl2 = new System.Windows.Forms.Label();
+            this.lbl3 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnKur
+            // 
+            this.btnKur.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.btnKur.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnKur.Location = new System.Drawing.Point(227, 277);
+            this.btnKur.Name = "btnKur";
+            this.btnKur.Size = new System.Drawing.Size(185, 56);
+            this.btnKur.TabIndex = 0;
+            this.btnKur.Text = "Döviz Kuru";
+            this.btnKur.UseVisualStyleBackColor = false;
+            this.btnKur.Click += new System.EventHandler(this.btnKur_Click);
+            // 
+            // lbl1
+            // 
+            this.lbl1.AutoSize = true;
+            this.lbl1.BackColor = System.Drawing.Color.White;
+            this.lbl1.Font = new System.Drawing.Font("Minion Pro SmBd", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl1.Location = new System.Drawing.Point(83, 175);
+            this.lbl1.Name = "lbl1";
+            this.lbl1.Size = new System.Drawing.Size(30, 19);
+            this.lbl1.TabIndex = 1;
+            this.lbl1.Text = "000";
+            // 
+            // lbl2
+            // 
+            this.lbl2.AutoSize = true;
+            this.lbl2.BackColor = System.Drawing.Color.White;
+            this.lbl2.Font = new System.Drawing.Font("Minion Pro SmBd", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl2.Location = new System.Drawing.Point(340, 175);
+            this.lbl2.Name = "lbl2";
+            this.lbl2.Size = new System.Drawing.Size(30, 19);
+            this.lbl2.TabIndex = 2;
+            this.lbl2.Text = "000";
+            // 
+            // lbl3
+            // 
+            this.lbl3.AutoSize = true;
+            this.lbl3.BackColor = System.Drawing.Color.White;
+            this.lbl3.Font = new System.Drawing.Font("Minion Pro SmBd", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl3.Location = new System.Drawing.Point(588, 175);
+            this.lbl3.Name = "lbl3";
+            this.lbl3.Size = new System.Drawing.Size(30, 19);
+            this.lbl3.TabIndex = 3;
+            this.lbl3.Text = "000";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(87, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(612, 37);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox2.Location = new System.Drawing.Point(87, 73);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(612, 37);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox3.Location = new System.Drawing.Point(87, 116);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(612, 37);
+            this.richTextBox3.TabIndex = 6;
+            this.richTextBox3.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.button1.Font = new System.Drawing.Font("Minion Pro SmBd", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(431, 277);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(194, 56);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Arşiv Döviz Kuru";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Exchange
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(848, 394);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.lbl3);
+            this.Controls.Add(this.lbl2);
+            this.Controls.Add(this.lbl1);
+            this.Controls.Add(this.btnKur);
+            this.Name = "Exchange";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnKur;
+        private System.Windows.Forms.Label lbl1;
+        private System.Windows.Forms.Label lbl2;
+        private System.Windows.Forms.Label lbl3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ desktop-exchange-rate/Exchange.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace desktop_exchange_rate
+{
+    public partial class Exchange : Form
+    {
+        public Exchange()
+        {
+            InitializeComponent();
+        }
+
+        private void btnKur_Click(object sender, EventArgs e)
+        {
+
+            string exchangeRate = "http://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(exchangeRate);
+
+            string today = "http://www.tcmb.gov.tr/kurlar/today.xml";
+            var xml = new XmlDocument();
+
+            xml.Load(today);
+
+            DateTime date = Convert.ToDateTime(xml.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value);
+
+            string USD = xml.SelectSingleNode("Tarih_Date/Currency [@Kod='USD']/BanknoteSelling").InnerXml;
+            richTextBox1.Text = string.Format("Tarih : {0} USD ; {1}", date.ToShortDateString(), USD);
+
+            string EURO = xml.SelectSingleNode("Tarih_Date/Currency [@Kod='EUR']/BanknoteSelling").InnerXml;
+            richTextBox2.Text = string.Format("Tarih : {0} EURO ; {1}", date.ToShortDateString(), EURO);
+
+            string POUND = xml.SelectSingleNode("Tarih_Date/Currency [@Kod='GBP']/BanknoteSelling").InnerXml;
+            richTextBox3.Text = string.Format("Tarih : {0} POUND ; {1}", date.ToShortDateString(), POUND);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string exchangeRate = "http://www.tcmb.gov.tr/kurlar/today.xml";
+            var xmlDoc = new XmlDocument();
+            xmlDoc.Load(exchangeRate);
+
+            var xml = new XmlDocument();
+            string backDay = "http://www.tcmb.gov.tr/kurlar/201001/15012010.xml";
+
+            
+            xml.Load(backDay);
+
+            DateTime date = Convert.ToDateTime(xml.SelectSingleNode("//Tarih_Date").Attributes["Tarih"].Value);
+
+            string USD = xml.SelectSingleNode("Tarih_Date/Currency [@Kod='USD']/BanknoteSelling").InnerXml;
+            lbl1.Text = string.Format("Tarih : {0} USD ; {1}", date.ToShortDateString(), USD);
+
+            string EURO = xml.SelectSingleNode("Tarih_Date/Currency [@Kod='EUR']/BanknoteSelling").InnerXml;
+            lbl2.Text = string.Format("Tarih : {0} EURO ; {1}", date.ToShortDateString(), EURO);
+
+            string POUND = xml.SelectSingleNode("Tarih_Date/Currency [@Kod='GBP']/BanknoteSelling").InnerXml;
+            lbl3.Text = string.Format("Tarih : {0} POUND ; {1}", date.ToShortDateString(), POUND);
+        }
+    }
+}
