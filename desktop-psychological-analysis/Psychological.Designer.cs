6fd31f33de4616d3e0859a32940912793e4a11fc
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-09 18:55:38 +0200

    desktop-psychological-analysis

--- desktop-psychological-analysis/Psychological.Designer.cs
+++ desktop-psychological-analysis/Psychological.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Psychological));
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
@@ -42,54 +43,56 @@
             this.btn1 = new System.Windows.Forms.Button();
             this.btnStart = new System.Windows.Forms.Button();
             this.txtQuestion = new System.Windows.Forms.TextBox();
+            this.axWindowsMediaPlayer1 = new AxWMPLib.AxWindowsMediaPlayer();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).BeginInit();
             this.SuspendLayout();
             // 
             // pictureBox5
             // 
-            this.pictureBox5.Location = new System.Drawing.Point(574, 160);
+            this.pictureBox5.Location = new System.Drawing.Point(553, 167);
             this.pictureBox5.Name = "pictureBox5";
-            this.pictureBox5.Size = new System.Drawing.Size(134, 97);
+            this.pictureBox5.Size = new System.Drawing.Size(180, 139);
             this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox5.TabIndex = 28;
             this.pictureBox5.TabStop = false;
             // 
             // pictureBox4
             // 
-            this.pictureBox4.Location = new System.Drawing.Point(423, 160);
+            this.pictureBox4.Location = new System.Drawing.Point(373, 167);
             this.pictureBox4.Name = "pictureBox4";
-            this.pictureBox4.Size = new System.Drawing.Size(134, 97);
+            this.pictureBox4.Size = new System.Drawing.Size(174, 139);
             this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox4.TabIndex = 27;
             this.pictureBox4.TabStop = false;
             // 
             // pictureBox3
             // 
-            this.pictureBox3.Location = new System.Drawing.Point(283, 160);
+            this.pictureBox3.Location = new System.Drawing.Point(189, 167);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(134, 97);
+            this.pictureBox3.Size = new System.Drawing.Size(178, 139);
             this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox3.TabIndex = 26;
             this.pictureBox3.TabStop = false;
             // 
             // pictureBox2
             // 
-            this.pictureBox2.Location = new System.Drawing.Point(143, 160);
+            this.pictureBox2.Location = new System.Drawing.Point(8, 167);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(134, 97);
+            this.pictureBox2.Size = new System.Drawing.Size(175, 139);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox2.TabIndex = 25;
             this.pictureBox2.TabStop = false;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Location = new System.Drawing.Point(3, 160);
+            this.pictureBox1.Location = new System.Drawing.Point(248, 118);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(134, 97);
+            this.pictureBox1.Size = new System.Drawing.Size(328, 188);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 24;
             this.pictureBox1.TabStop = false;
@@ -97,84 +100,109 @@
             // lblTotal
             // 
             this.lblTotal.AutoSize = true;
-            this.lblTotal.Location = new System.Drawing.Point(758, 112);
+            this.lblTotal.Font = new System.Drawing.Font("Minion Pro SmBd", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblTotal.Location = new System.Drawing.Point(782, 128);
             this.lblTotal.Name = "lblTotal";
-            this.lblTotal.Size = new System.Drawing.Size(19, 13);
+            this.lblTotal.Size = new System.Drawing.Size(30, 26);
             this.lblTotal.TabIndex = 23;
             this.lblTotal.Text = "00";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(733, 85);
+            this.label1.Font = new System.Drawing.Font("Minion Pro SmBd", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(736, 86);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(70, 13);
+            this.label1.Size = new System.Drawing.Size(117, 26);
             this.label1.TabIndex = 22;
             this.label1.Text = "Toplam Puan";
             // 
             // btn5
             // 
-            this.btn5.Location = new System.Drawing.Point(3, 411);
+            this.btn5.BackColor = System.Drawing.Color.Transparent;
+            this.btn5.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn5.Location = new System.Drawing.Point(6, 460);
             this.btn5.Name = "btn5";
-            this.btn5.Size = new System.Drawing.Size(711, 31);
+            this.btn5.Size = new System.Drawing.Size(844, 31);
             this.btn5.TabIndex = 21;
-            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.UseVisualStyleBackColor = false;
             // 
             // btn4
             // 
-            this.btn4.Location = new System.Drawing.Point(3, 374);
+            this.btn4.BackColor = System.Drawing.Color.Transparent;
+            this.btn4.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn4.Location = new System.Drawing.Point(6, 423);
             this.btn4.Name = "btn4";
-            this.btn4.Size = new System.Drawing.Size(711, 31);
+            this.btn4.Size = new System.Drawing.Size(844, 31);
             this.btn4.TabIndex = 20;
-            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.UseVisualStyleBackColor = false;
             // 
             // btn3
             // 
-            this.btn3.Location = new System.Drawing.Point(3, 337);
+            this.btn3.BackColor = System.Drawing.Color.Transparent;
+            this.btn3.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn3.Location = new System.Drawing.Point(6, 386);
             this.btn3.Name = "btn3";
-            this.btn3.Size = new System.Drawing.Size(711, 31);
+            this.btn3.Size = new System.Drawing.Size(844, 31);
             this.btn3.TabIndex = 19;
-            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.UseVisualStyleBackColor = false;
             // 
             // btn2
             // 
-            this.btn2.Location = new System.Drawing.Point(3, 300);
+            this.btn2.BackColor = System.Drawing.Color.Transparent;
+            this.btn2.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn2.Location = new System.Drawing.Point(6, 349);
             this.btn2.Name = "btn2";
-            this.btn2.Size = new System.Drawing.Size(711, 31);
+            this.btn2.Size = new System.Drawing.Size(844, 31);
             this.btn2.TabIndex = 18;
-            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.UseVisualStyleBackColor = false;
             // 
             // btn1
             // 
-            this.btn1.Location = new System.Drawing.Point(3, 263);
+            this.btn1.BackColor = System.Drawing.Color.Transparent;
+            this.btn1.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btn1.Location = new System.Drawing.Point(6, 312);
             this.btn1.Name = "btn1";
-            this.btn1.Size = new System.Drawing.Size(711, 31);
+            this.btn1.Size = new System.Drawing.Size(844, 31);
             this.btn1.TabIndex = 17;
-            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.UseVisualStyleBackColor = false;
             // 
             // btnStart
             // 
-            this.btnStart.Location = new System.Drawing.Point(720, 19);
+            this.btnStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnStart.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnStart.Location = new System.Drawing.Point(741, 12);
             this.btnStart.Name = "btnStart";
             this.btnStart.Size = new System.Drawing.Size(96, 31);
             this.btnStart.TabIndex = 16;
             this.btnStart.Text = "Teste Başla";
-            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.UseVisualStyleBackColor = false;
             // 
             // txtQuestion
             // 
+            this.txtQuestion.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
             this.txtQuestion.Location = new System.Drawing.Point(3, 8);
             this.txtQuestion.Multiline = true;
             this.txtQuestion.Name = "txtQuestion";
-            this.txtQuestion.Size = new System.Drawing.Size(711, 249);
+            this.txtQuestion.Size = new System.Drawing.Size(727, 104);
             this.txtQuestion.TabIndex = 15;
+            // 
+            // axWindowsMediaPlayer1
+            // 
+            this.axWindowsMediaPlayer1.Enabled = true;
+            this.axWindowsMediaPlayer1.Location = new System.Drawing.Point(13, 8);
+            this.axWindowsMediaPlayer1.Name = "axWindowsMediaPlayer1";
+            this.axWindowsMediaPlayer1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWindowsMediaPlayer1.OcxState")));
+            this.axWindowsMediaPlayer1.Size = new System.Drawing.Size(75, 23);
+            this.axWindowsMediaPlayer1.TabIndex = 29;
             // 
             // Psychological
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(859, 502);
+            this.Controls.Add(this.axWindowsMediaPlayer1);
             this.Controls.Add(this.pictureBox5);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
@@ -196,6 +224,7 @@
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.axWindowsMediaPlayer1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -217,6 +246,7 @@
         private System.Windows.Forms.Button btn1;
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.TextBox txtQuestion;
+        private AxWMPLib.AxWindowsMediaPlayer axWindowsMediaPlayer1;
     }
 }
 
