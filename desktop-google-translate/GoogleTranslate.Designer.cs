6c5d5397264f3ba1569b0b77c63e3ac30b575bcc
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-10 20:31:32 +0200

    desktop-google-translate

---
+++ desktop-google-translate/GoogleTranslate.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace desktop_google_translate
+{
+    partial class GoogleTranslate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnTranslate = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.radioBtnEng = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.radioBtnTr = new System.Windows.Forms.RadioButton();
+            this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // btnTranslate
+            // 
+            this.btnTranslate.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.btnTranslate.Font = new System.Drawing.Font("Minion Pro SmBd", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnTranslate.Location = new System.Drawing.Point(204, 192);
+            this.btnTranslate.Name = "btnTranslate";
+            this.btnTranslate.Size = new System.Drawing.Size(114, 46);
+            this.btnTranslate.TabIndex = 0;
+            this.btnTranslate.Text = "Çevir";
+            this.btnTranslate.UseVisualStyleBackColor = false;
+            this.btnTranslate.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox1.Location = new System.Drawing.Point(3, 30);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(265, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // radioBtnEng
+            // 
+            this.radioBtnEng.AutoSize = true;
+            this.radioBtnEng.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioBtnEng.Location = new System.Drawing.Point(253, 141);
+            this.radioBtnEng.Name = "radioBtnEng";
+            this.radioBtnEng.Size = new System.Drawing.Size(95, 26);
+            this.radioBtnEng.TabIndex = 2;
+            this.radioBtnEng.TabStop = true;
+            this.radioBtnEng.Text = "ENG - TR";
+            this.radioBtnEng.UseVisualStyleBackColor = true;
+            this.radioBtnEng.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(12, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 22);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Türkçe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(280, 8);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "İngilizce";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.richTextBox2.Location = new System.Drawing.Point(274, 30);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(260, 96);
+            this.richTextBox2.TabIndex = 5;
+            this.richTextBox2.Text = "";
+            this.richTextBox2.TextChanged += new System.EventHandler(this.richTextBox2_TextChanged);
+            // 
+            // radioBtnTr
+            // 
+            this.radioBtnTr.AutoSize = true;
+            this.radioBtnTr.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.radioBtnTr.Location = new System.Drawing.Point(12, 141);
+            this.radioBtnTr.Name = "radioBtnTr";
+            this.radioBtnTr.Size = new System.Drawing.Size(95, 26);
+            this.radioBtnTr.TabIndex = 6;
+            this.radioBtnTr.TabStop = true;
+            this.radioBtnTr.Text = "TR - ENG";
+            this.radioBtnTr.UseVisualStyleBackColor = true;
+            this.radioBtnTr.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // webBrowser1
+            // 
+            this.webBrowser1.Location = new System.Drawing.Point(432, 362);
+            this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser1.Name = "webBrowser1";
+            this.webBrowser1.Size = new System.Drawing.Size(93, 34);
+            this.webBrowser1.TabIndex = 7;
+            // 
+            // GoogleTranslate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(537, 283);
+            this.Controls.Add(this.webBrowser1);
+            this.Controls.Add(this.radioBtnTr);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioBtnEng);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnTranslate);
+            this.Name = "GoogleTranslate";
+            this.Text = "Translate";
+            this.Load += new System.EventHandler(this.GoogleTranslate_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnTranslate;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RadioButton radioBtnEng;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RadioButton radioBtnTr;
+        private System.Windows.Forms.WebBrowser webBrowser1;
+    }
+}
+
---
+++ desktop-google-translate/GoogleTranslate.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+namespace desktop_google_translate
+{
+    public partial class GoogleTranslate : Form
+    {
+        public GoogleTranslate()
+        {
+            InitializeComponent();
+        }
+        public bool test()
+        {
+            string address = "http://www.google.com";
+            try
+            {
+                WebRequest istek = WebRequest.Create(address);
+                WebResponse yanit = istek.GetResponse();
+
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            return true;
+        }
+
+        private void GoogleTranslate_Load(object sender, EventArgs e)
+        {
+            if (test())
+            {
+                this.Text = "Bağlantı var.";
+            }
+            else
+            {
+                this.Text = "Bağlantı yok.";
+            }
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://translate.google.com.tr/#tr/en/");
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Navigate("https://translate.google.com.tr/#en/tr/");
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Document.GetElementById("Source").InnerText = richTextBox1.Text;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (radioBtnTr.Checked == true)
+            {
+                richTextBox1.Text = webBrowser1.Document.GetElementById("Result_Box").InnerText;
+            }
+            if (radioBtnEng.Checked == true)
+            {
+                richTextBox2.Text = webBrowser1.Document.GetElementById("Result_Box").InnerText;
+            }
+        }
+
+        private void richTextBox2_TextChanged(object sender, EventArgs e)
+        {
+            webBrowser1.Document.GetElementById("Source").InnerText = richTextBox2.Text;
+        }
+    }
+}
