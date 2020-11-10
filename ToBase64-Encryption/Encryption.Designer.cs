aeba12d676b8663b12d5b03162dc329fa182fdb1
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-10 22:45:43 +0200

    ToBase64-Encryption

---
+++ ToBase64-Encryption/Encryption.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace ToBase64_Encryption
+{
+    partial class Encryption
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPassword = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxMetin = new System.Windows.Forms.TextBox();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnPasswordSolve = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnPassword
+            // 
+            this.btnPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnPassword.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPassword.Location = new System.Drawing.Point(149, 116);
+            this.btnPassword.Name = "btnPassword";
+            this.btnPassword.Size = new System.Drawing.Size(203, 34);
+            this.btnPassword.TabIndex = 0;
+            this.btnPassword.Text = "Şifrele";
+            this.btnPassword.UseVisualStyleBackColor = false;
+            this.btnPassword.Click += new System.EventHandler(this.btnPassword_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(61, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Metin : ";
+            // 
+            // textBoxMetin
+            // 
+            this.textBoxMetin.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxMetin.Location = new System.Drawing.Point(139, 25);
+            this.textBoxMetin.Name = "textBoxMetin";
+            this.textBoxMetin.Size = new System.Drawing.Size(228, 27);
+            this.textBoxMetin.TabIndex = 2;
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxPassword.Location = new System.Drawing.Point(139, 61);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(228, 27);
+            this.textBoxPassword.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(66, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 22);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Şifre  : ";
+            // 
+            // btnPasswordSolve
+            // 
+            this.btnPasswordSolve.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.btnPasswordSolve.Font = new System.Drawing.Font("Minion Pro SmBd", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnPasswordSolve.Location = new System.Drawing.Point(149, 169);
+            this.btnPasswordSolve.Name = "btnPasswordSolve";
+            this.btnPasswordSolve.Size = new System.Drawing.Size(203, 34);
+            this.btnPasswordSolve.TabIndex = 5;
+            this.btnPasswordSolve.Text = "Şifre Çöz ";
+            this.btnPasswordSolve.UseVisualStyleBackColor = false;
+            this.btnPasswordSolve.Click += new System.EventHandler(this.btnPasswordSolve_Click);
+            // 
+            // Encryption
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(488, 271);
+            this.Controls.Add(this.btnPasswordSolve);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxMetin);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnPassword);
+            this.Name = "Encryption";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPassword;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxMetin;
+        private System.Windows.Forms.TextBox textBoxPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnPasswordSolve;
+    }
+}
+
---
+++ ToBase64-Encryption/Encryption.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ToBase64_Encryption
+{
+    public partial class Encryption : Form
+    {
+        public Encryption()
+        {
+            InitializeComponent();
+        }
+
+        private void btnPassword_Click(object sender, EventArgs e)
+        {
+            string text = textBoxMetin.Text;
+            byte[] data = ASCIIEncoding.ASCII.GetBytes(text);
+
+            string PasswordText = Convert.ToBase64String(data);
+            textBoxPassword.Text = PasswordText;
+
+        }
+
+        private void btnPasswordSolve_Click(object sender, EventArgs e)
+        {
+            string textSolve = textBoxPassword.Text;
+            byte[] dataSolve = Convert.FromBase64String(textSolve);
+
+            string mainText = ASCIIEncoding.ASCII.GetString(dataSolve);
+            textBoxMetin.Text = mainText;
+        }
+    }
+}
