bae2c491bb7e0be674b52f24b5cbfbd5137dbe9e
https://dev.azure.com/KARTeams/ <uremsancaktutan@outlook.com>
2020-11-11 00:03:45 +0200

    qr-code-barcode

---
+++ qr-code-barcode/QRBarcode.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace qr_code_barcode
+{
+    partial class QRBarcode
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnQR = new System.Windows.Forms.Button();
+            this.textBoxQR = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBoxBarcode = new System.Windows.Forms.TextBox();
+            this.btnBarcode = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnQR
+            // 
+            this.btnQR.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btnQR.Font = new System.Drawing.Font("Minion Pro SmBd", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnQR.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnQR.Location = new System.Drawing.Point(29, 62);
+            this.btnQR.Name = "btnQR";
+            this.btnQR.Size = new System.Drawing.Size(82, 30);
+            this.btnQR.TabIndex = 0;
+            this.btnQR.Text = "QR Code";
+            this.btnQR.UseVisualStyleBackColor = false;
+            this.btnQR.Click += new System.EventHandler(this.btnQR_Click);
+            // 
+            // textBoxQR
+            // 
+            this.textBoxQR.Font = new System.Drawing.Font("Minion Pro SmBd", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxQR.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxQR.Location = new System.Drawing.Point(29, 36);
+            this.textBoxQR.Name = "textBoxQR";
+            this.textBoxQR.Size = new System.Drawing.Size(191, 25);
+            this.textBoxQR.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(29, 111);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(191, 156);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(252, 111);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(191, 156);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBoxBarcode
+            // 
+            this.textBoxBarcode.Font = new System.Drawing.Font("Minion Pro SmBd", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.textBoxBarcode.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.textBoxBarcode.Location = new System.Drawing.Point(252, 36);
+            this.textBoxBarcode.Name = "textBoxBarcode";
+            this.textBoxBarcode.Size = new System.Drawing.Size(191, 25);
+            this.textBoxBarcode.TabIndex = 4;
+            // 
+            // btnBarcode
+            // 
+            this.btnBarcode.BackColor = System.Drawing.Color.Gold;
+            this.btnBarcode.Font = new System.Drawing.Font("Minion Pro SmBd", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBarcode.ForeColor = System.Drawing.Color.White;
+            this.btnBarcode.Location = new System.Drawing.Point(252, 62);
+            this.btnBarcode.Name = "btnBarcode";
+            this.btnBarcode.Size = new System.Drawing.Size(79, 30);
+            this.btnBarcode.TabIndex = 3;
+            this.btnBarcode.Text = "BarCode";
+            this.btnBarcode.UseVisualStyleBackColor = false;
+            this.btnBarcode.Click += new System.EventHandler(this.btnBarcode_Click);
+            // 
+            // QRBarcode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(476, 286);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBoxBarcode);
+            this.Controls.Add(this.btnBarcode);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBoxQR);
+            this.Controls.Add(this.btnQR);
+            this.Name = "QRBarcode";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnQR;
+        private System.Windows.Forms.TextBox textBoxQR;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBoxBarcode;
+        private System.Windows.Forms.Button btnBarcode;
+    }
+}
+
---
+++ qr-code-barcode/QRBarcode.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+
+namespace qr_code_barcode
+{
+    public partial class QRBarcode : Form
+    {
+        public QRBarcode()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnQR_Click(object sender, EventArgs e)
+        {
+            QRCodeEncoder qRCode = new QRCodeEncoder();
+            pictureBox1.Image = qRCode.Encode(textBoxQR.Text);
+        }
+
+        private void btnBarcode_Click(object sender, EventArgs e)
+        {
+            Zen.Barcode.Code128BarcodeDraw barcodeDraw = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            pictureBox2.Image = barcodeDraw.Draw(textBoxBarcode.Text,100);
+        }
+    }
+}
